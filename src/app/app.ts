20a24a96ed16f45f4cbad97bcc33122694efd177
Denis Tineo <frezzado@alumnes.ilerna.com>
2026-09-23 16:39:28 +0200

    a

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Producte } from './interfaces/producte';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,29 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('angular-entorns-2627');
+
+  prod1 : Producte = {
+    id: 1,
+    nom: 'a',
+    preu: 5,
+    disponible: true
+  };
+
+  prod2 : Producte = {
+    id: 2,
+    nom: 'b',
+    preu: 6,
+    disponible: false
+  };
+
+  prod3 : Producte = {
+    id: 3,
+    nom: 'y',
+    preu: 15,
+    disponible: true
+  };
+
+  biblioteca : Producte[] = [this.prod1, this.prod2, this.prod3];
+
+
 }
---
+++ src/app/interfaces/producte.ts
@@ -0,0 +1,7 @@
+export interface Producte{
+	id: number;
+	nom: string;
+	preu: number;
+	disponible: boolean;
+	descripcio ?: string //? é para fazer ese lixo opcional
+}
